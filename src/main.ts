57fd10ba1a351927c0d484b53d08ef61cdaac82c
Kiminni <imk0980@gmail.com>
2024-10-11 19:12:26 +0900

    chore: 초기 세팅

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { initializeTransactionalContext } from 'typeorm-transactional';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   initializeTransactionalContext();
 
   const app = await NestFactory.create(AppModule);
-  // TODO: 프로그램 구현
+  const config = new DocumentBuilder()
+    .setTitle('휴대폰 인증 API')
+    .setDescription('휴대폰 인증 API 문서입니다.')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(process.env.PORT || 8000);
 
   console.log(`Application is running on: ${await app.getUrl()}`);
