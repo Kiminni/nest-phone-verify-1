834ce8251f0b292350d882c985154bdcfb71402e
Kiminni <imk0980@gmail.com>
2024-10-11 21:26:40 +0900

    chore: GlobalPipe 추가

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,21 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { initializeTransactionalContext } from 'typeorm-transactional';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   initializeTransactionalContext();
 
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true, // 이 옵션이 있어야 합니다.
+    }),
+  );
+
   const config = new DocumentBuilder()
     .setTitle('휴대폰 인증 API')
     .setDescription('휴대폰 인증 API 문서입니다.')
