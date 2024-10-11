0ff581c200d136df395aaae8bf86f8caed3e0d4e
Kiminni <imk0980@gmail.com>
2024-10-11 21:26:19 +0900

    fix: dto 메시지 변경

--- src/dto/send-code.dto.ts
+++ src/dto/send-code.dto.ts
@@ -1,13 +1,13 @@
-import { IsNotEmpty, IsString, Matches } from "class-validator";
-import { ApiProperty } from "@nestjs/swagger";
+import { IsNotEmpty, IsString, Matches } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class SendCodeDto {
   @ApiProperty({
     example: '010-1234-5678',
-    description: '전화번호'
+    description: '전화번호',
   })
   @Matches(/^010-\d{4}-\d{4}$/, {
-    message: '휴대전화 번호는 반드시 형식에 맞게 작성해야합니다.',
+    message: '휴대전화 번호 형식을 맞춰주세요.',
   })
   @IsNotEmpty()
   @IsString()
