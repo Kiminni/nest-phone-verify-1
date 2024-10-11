fe2e18dbce43a446336e5f895f41448ff131ea2a
Kiminni <imk0980@gmail.com>
2024-10-11 20:47:20 +0900

    feat: sendCode, verifyCode dto 생성

---
+++ src/dto/verify-code.dto.ts
@@ -0,0 +1,23 @@
+import { IsNotEmpty, IsString, Length, Matches } from "class-validator";
+import { ApiProperty } from "@nestjs/swagger";
+
+export class VerifyCodeDto {
+  @IsNotEmpty()
+  @ApiProperty({
+    example: '010-1234-5678',
+    description: '전화번호'
+  })
+  @Matches(/^010-\d{4}-\d{4}$/, {
+    message: '휴대전화 번호는 반드시 형식에 맞게 작성해야합니다.',
+  })
+  phoneNumber: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @ApiProperty({
+    example: '123456',
+    description: '인증코드'
+  })
+  @Length(6, 6, { message: '코드는 여섯자리입니다.' })
+  code: string;
+}
